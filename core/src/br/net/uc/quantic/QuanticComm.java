f375c1e62280b0121dfb4b409b193f3f68dc2c4b
Daniel Sampaio <daniel.sampaio@accurate.com.br>
2020-10-03 11:47:09 -0300

    # Renderizando os planetas na tela

--- core/src/br/net/uc/quantic/QuanticComm.java
+++ core/src/br/net/uc/quantic/QuanticComm.java
@@ -1,34 +1,60 @@
 package br.net.uc.quantic;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import br.net.uc.quantic.entity.EarthEntity;
+import br.net.uc.quantic.entity.MarsEntity;
+import br.net.uc.quantic.entity.WorldEntity;
+import br.net.uc.quantic.system.RenderSystem;
+
 public class QuanticComm extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture img;
+	Engine engine;
+
+	EarthEntity earth;
+	MarsEntity mars;
+
+	WorldEntity world;
+
+	RenderSystem renderSystem;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		engine = new Engine();
+
+		earth = new EarthEntity();
+		engine.addEntity(earth);
+
+		mars = new MarsEntity();
+		engine.addEntity(mars);
+
+		world = new WorldEntity();
+		engine.addEntity(world);
+
+		renderSystem = new RenderSystem();
+		engine.addSystem(renderSystem);
+		renderSystem.setProcessing(true);
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		engine.update(Gdx.graphics.getDeltaTime());
+
+
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+	public void dispose () {}
 }
